76e1eda230aaa8d5203996ad1208b0dfc4bdbcd0
JosephEmeka <josephemekaonah@gmail.com>
2023-11-23 10:01:54 +0100

    Update investment.py

--- investment.py
+++ investment.py
@@ -1,5 +1,3 @@
-
-
 principal = int(input("Enter principal(original amount invested): "))
 annualReturnRate = int(input("Enter annual rate of return: "))
 numberOfYears = int(input("Enter number of years (10, 20 or 30)   : "))
